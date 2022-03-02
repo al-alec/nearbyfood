28830ff694b27b9623960ed1944a431118e57831
al-alec <yehadjialexis@gmail.com>
2022-03-02 07:54:09 +0000

    first commit

---
+++ config/telegraph.php
@@ -0,0 +1,26 @@
+<?php
+
+use DefStudio\Telegraph\Handlers\EmptyWebhookHandler;
+use DefStudio\Telegraph\Telegraph;
+
+return [
+    /*
+     * Sets Telegraph messages default parse mode
+     * allowed values: html|markdown
+     */
+    'default_parse_mode' => Telegraph::PARSE_HTML,
+
+    /*
+     * Sets the handler to be used when Telegraph
+     * receives a new webhook call.
+     *
+     * For reference, see https://def-studio.github.io/telegraph/webhooks/overview
+     */
+    'webhook_handler' => EmptyWebhookHandler::class,
+
+    /*
+     * If enabled, Telegraph dumps received
+     * webhook messages to logs
+     */
+    'debug_mode' => true,
+];
