daeb74ff1940dcd5be3855b1e263a6628b6e6e64
al-alec <yehadjialexis@gmail.com>
2022-03-02 10:49:25 +0000

    first call

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -47,11 +47,36 @@ class TelegramBotController extends WebhookHandler
 
 
     $this->chat->html($html)
-//      ->keyboard(
-//      Keyboard::make()->buttons([
-//          Button::make('Je veux manger')->action('delete')->param('id', '42'),
-//          Button::make('Je veux passer du temps quelque part')->url('https://test.it'),
-//    ]))
+      ->keyboard(
+      Keyboard::make()->buttons([
+          Button::make('Je veux manger')->action('delete')->param('id', '42'),
+          Button::make('Je veux passer du temps quelque part')->url('https://test.it'),
+    ]))
+      ->send();
+  }
+
+  public function start()
+  {
+    $html = "<b>Salut, moi c'est alec</b> 🤖 \n
+      \n
+      Vous avez faim et vous etes fatigué de manger les meme bouffe chaque jour 😫?
+
+      Vous n'avez pas un grand budget mais vous voulez bien manger 🥯🥗 \n
+
+      \n
+      Je vais a trouver les restaurants, ou petit vendeur ( ayimolou, veyi, Frites, Spaghetti, Poisson braisé, pinon etc..) les plus proche de vous 🍴😋
+
+      \n Vous recherchez egalement un endroit calme pour passer du temps avec votre partenaire? 😍🥰👩‍❤️‍👨  Ou encore un(e) ami(e) pour discuter affaire? 💵📧
+
+      \n Je vais vous aider quelque soit votre budget, alors let's go. Explorez par vous meme";
+
+
+    $this->chat->html($html)
+      ->keyboard(
+      Keyboard::make()->buttons([
+          Button::make('Je veux manger')->action('delete')->param('id', '42'),
+          Button::make('Je veux passer du temps quelque part')->url('https://test.it'),
+    ]))
       ->send();
   }
 
